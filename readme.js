21ddb649489805c4fbeb1c7eee3902a9fde9224c
Ranjith-FSD <ranjithtechstar@gmail.com>
2023-11-23 13:27:43 +0530

    readme.js file Step by step add in Github

---
+++ readme.js
@@ -0,0 +1,19 @@
+/*
+
+1. Open terminal in mac or linux & command prompt in windows
+    2. dir command - list all files and folders in current directory
+    3. cd command - change directory
+    4. cd .. - go back to parent directory
+    5. cd Desktop or cd Documents - go to Desktop or Documents directory
+    6. mkdir foldername command - create a new directory
+    7. cd foldername - go to foldername directory
+    8. open the folder in vscode - either code . or open visual studio code and open the folder
+    9. check the version of the git installed by 
+        - open the terminal inside vscode from terminal menu
+        - git --version or git -v
+    10. open github.com and create a new repository
+        - give the repository name
+        - leave the visibility as public
+        - create repository
+
+*/
